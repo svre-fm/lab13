0ed448e8329cc0004a49b477c3ff74d55ce85f83
svre-fm <firmoioi96@gmail.com>
2024-12-27 15:11:21 +0700

    complete 1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,37 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double num[],int N ,double ans[]){
+    double sum = 0;
+    for(int i = 0; i < N ; i++){
+        sum += num[i];
+    }
+    ans[0] = sum/N;
+
+    double suminsqr = 0;
+    for(int i = 0; i < N; i++){
+        suminsqr += pow(num[i]-ans[0],2);
+    }
+    ans[1] = sqrt(suminsqr/N);
+
+    double sumGM = 1;
+    for(int i = 0; i < N;i++){
+        sumGM *= num[i];
+    }
+    ans[2] = pow(sumGM,1.00/N);
+
+    double sumHM = 0;
+    for(int i = 0; i < N;i++){
+        sumHM += 1.0/num[i];
+    }
+    ans[3] = N/sumHM;
+
+    double max = num[0],min = num[0];
+    for(int i = 0; i < N;i++){
+        if(num[i] >= max) max = num[i];
+        if(num[i] < min) min = num[i];
+    }
+    ans[4] = max;
+    ans[5] = min;
+}
